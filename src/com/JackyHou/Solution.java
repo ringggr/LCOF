06c1698721bd838f8f9f379665340117f23298d7
ringggr <ringggr@icloud.com>
2020-04-15 18:32:38 +0800

    42. 连续子数组的最大和

--- src/com/JackyHou/Solution.java
+++ src/com/JackyHou/Solution.java
@@ -1078,5 +1078,16 @@ public class Solution {
     }
 
     // 41. 数据流中的中位数
+    // 见MedianFinder类
+
+    // 42. 连续子数组的最大和
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(nums[i], res);
+        }
+        return res;
+    }
 
 }
