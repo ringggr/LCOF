fa40af0b30b84beb3a26309cd71686808404bd4b
ringggr <ringggr@icloud.com>
2020-04-30 20:38:30 +0800

    63. 不同路径 II 存在障碍物

--- src/com/JackyHou/Solution.java
+++ src/com/JackyHou/Solution.java
@@ -178,6 +178,39 @@ public class Solution {
 
     // 63. 不同路径 II
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int row = obstacleGrid.length, col = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[row - 1][col- 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[row][col];
 
+        for (int i = 0; i < row; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < col; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            }
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
 }
