47895b30df74a498e323580069d64afae0eb3dba
ringggr <ringggr@icloud.com>
2020-04-30 20:43:23 +0800

    63. 不同路径 II 存在障碍物

--- src/com/JackyHou/Solution.java
+++ src/com/JackyHou/Solution.java
@@ -213,4 +213,27 @@ public class Solution {
 
         return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
+
+    // 64. 最小路径和
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int j = 1; j < cols; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[rows - 1][cols - 1];
+    }
 }
